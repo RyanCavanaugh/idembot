bf9d6e0c9bb14aab7e9ce0883993bbd1bd1739ce
Andy Hanson <anhans@microsoft.com>
2017-08-16 11:21:23 -0700

    Remove unused packages

--- src/logging.ts
+++ src/logging.ts
@@ -1,12 +1,10 @@
-// import * as bragi from 'bragi';
-
 export interface Log {
     log(message: string, data?: any): void;
 }
 
 export function get(_groupName: string): Log {
     return {
-        log: (message: string, data?: any) =>  console.log(message, data)// bragi.log(message, data)
+        log: (message: string, data?: any) =>  console.log(message, data),
     };
 }
 
