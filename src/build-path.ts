7632bc4ae01b2088db4fd959918a222dd4fcc4a8
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-10-31 10:40:14 -0700

    Fix type error

--- src/build-path.ts
+++ src/build-path.ts
@@ -1,3 +1,3 @@
 export default function path(...parts: Array<string | number>): string {
-    return "/" + parts.map(encodeURIComponent).join("/");
+    return "/" + parts.map(p => p.toString()).map(encodeURIComponent).join("/");
 }
